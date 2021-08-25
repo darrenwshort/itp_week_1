4bb45cfaa2e8e43de92ba62e309eaa91444d723f
darrenwshort <w1765868@apps.losrios.edu>
2021-08-25 12:48:02 -0700

    finished exercise.py for day 2 class

--- day_2/exercise.py
+++ day_2/exercise.py
@@ -1,13 +1,27 @@
 # ITP Week 1 Day 2 Exercise
 
 # Take an user's input for his age
+age = input("Enter your age: ")
 
 # The user input comes in as a string so we have to cast it to a int!
+if(age.isnumeric()):   # test that input string has only 'numbers'.  This also handles negative values.
+    age = int(age)
+else:
+    print("You entered an invalid age.  Please try again.")
+    exit()
 
 # Use an if/else to determine if they are of legal drinking age.
 # if the user is of age, print "Welcome!"
 # else, tell them to come back in X amount of years (use math operations)
+legal_drinking_age = 21
+if(age >= legal_drinking_age):
+    print("Welcome, drink up!")
+else:
+    years = legal_drinking_age - age
+    print("You're too young for a drink.   Please come back in " + str(years) + " years, young buck!")
 
 # Bonus: Add a validation by checking the type of the user input
 # to ensure it can be casted as an int. Handle any other input that
 # are not numbers to try again.
+
+#####  SEE bonus code attempt above. ~Darren ######
--- day_2/practice_2.py
+++ day_2/practice_2.py
@@ -10,26 +10,32 @@
 # Assign a new variable for each step
 # at the end, use an if statement to see if the final is always 5!
 
-
+print()
 user_input = int(input("Enter number from 1 - 9: "))
+
+# test if non-int, eg: str
 if(not isinstance(user_input, int)):
     print("You must enter an integer value from 1 - 9.")
     exit()
 
+# test range
 if(user_input < 1 or user_input > 9):
     print("You must enter a number from 1 - 9 only.  Please restart program.")
     exit()
+
 
 multiplied = user_input * 2
 added = multiplied + 10
 divided = added / 2
 final_num = divided - user_input
 
+# test final # is indeed 5
 if(final_num == 5):
     print("Final number is 5.  You did it!")
 else:
     print("Final number was something other than 5.  Please try again.")
 
+print()
 
 
 
