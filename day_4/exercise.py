81e480fc9de3baddbb4757eea75780ad6de2f733
darrenwshort <w1765868@apps.losrios.edu>
2021-08-30 18:58:10 -0700

    completed week 1 day 4 exercise

--- day_4/exercise.py
+++ day_4/exercise.py
@@ -12,14 +12,22 @@ inventory = {
 
 # SCENARIO: A person came in and bought one of everything!
 
-for item in inventory:
-    pass
+for item in inventory.items():
     # decrement item by using an assignment operator (Day 2 Lecture line #130)
-
+    inventory.update({item[0]: item[1] + 1})
     # NOTE: recall that item represents the key of the key:value pair
+print(inventory) #print changes after increment of values
 
 # SCENARIO: REMOVE ANY 0 ITEMS
 
-for item in inventory:
-    pass
+del_list = []j   #create empty list
+for item in inventory.items():
     # use an if statement to check if the value is 0 and then remove it
+    if(item[1] == 0):
+        del_list.append(item[0])  #build list of keys that need to be deleted
+
+for k in del_list:        #loop through list of items to be deleted, by key
+    del inventory[k]      #delete item based on current k
+
+print(inventory)          #print inventory to show if any items where deleted.
+
